557e2619aaa429a00445e3cc063c383b0bff90c3
raimop <ray@tlu.ee>
2018-11-23 01:09:39 +0200

    v1.2

--- sdh.bat
+++ sdh.bat
@@ -1,16 +1,17 @@
 @echo off & setlocal enabledelayedexpansion
 :start
 cls
-echo Shutdown helper v1.1 by Raimo (Tallinn University OS subject homework 18 Nov 2018, updated 21 Nov 2018^)
-echo Press ENTER to continue with the script
+echo Shutdown helper v1.2 by Raimo (Tallinn University OS subject homework, last updated 23 Nov 2018^)
+echo To continue with the script, simply press Enter
 echo To abort any previous planned shutdowns, write: a
 echo For help, write: /?
 echo.
 
-:initial
+:initialpart
+set "initial="
 set /p initial="Your wish is my command: "
 
-if "%initial%" equ "a" (
+if "!initial!" equ "a" (
 echo Shutdown abort initiated successfully, unless shutdown prompted an error one line below this one.
 echo.
 shutdown -a
@@ -24,20 +25,20 @@ echo To abort any planned shutdowns, write: a
 echo To prompt this help again, write: /?
 echo To continue with SDH (Shutdown Helper^), simply press Enter key
 echo.
-goto :initial
+goto :initialpart
 )
 
-if "%initial%" equ "" (
+if "!initial!" equ "" (
 echo Enter pressed 
 goto :mainpart
 )
 
-if "%initial%" lss "" (
+if "!initial!" lss 0 (
 echo less than ""
 goto :start
 )
 
-if "%initial%" gtr "" (
+if "!initial!" gtr 0 (
 echo greater than ""
 goto :start
 )
@@ -54,13 +55,14 @@ echo shutdown in !calc! seconds (!inMin! minutes^) - select %%i
 )
 
 :select
-set /p choice="Select number "
+set "choice="
+set /p choice="Select number: "
 if !choice! lss 0 (
-echo You have entered a number below 0, going back
+echo Choose a number from 0 to 12, going back
 goto :select
 )
 if !choice! gtr 12 (
-echo You have entered a number above 12, going back
+echo Choose a number from 0 to 12, going back
 goto :select
 )
 
@@ -71,31 +73,34 @@ SET /a seconds=300*%%i
 SET /a minutes=!seconds!/60
 echo.
 echo You chose %%i, will shutdown in !seconds! seconds (!minutes! minutes^)
-shutdown -s -t !time[%%i]! -c "Shutdown in !time[%%i]! seconds by SDH script (by Raimo)" 
+echo.
+shutdown -s -t !time[%%i]! -c "Shutdown in !time[%%i]! seconds by SDH script (by Raimo)"
+echo.
 )
 )
 
 :lastchance
-echo If you want to re-run this script, write: yes
-echo If you are done and want to exit, press Enter
+echo If you want to re-run this script, write: 1
+echo If you are done and want to exit, simply press Enter
 
+set "lastChance="
 set /p lastChance="Choice: "
 
-if "%lastChance%" == "yes" (
+if "!lastChance!" == "1" (
 goto :start
 )
 
-if "%lastChance%" == "" (
+if "!lastChance!" == "" (
 echo Goodbye^^!
 timeout /t 5
 goto :oef
 )
 
-if "%lastChance%" lss "" (
+if "!lastChance!" lss 0 (
 goto :lastchance
 )
 
-if "%lastChance%" gtr "" (
+if "!lastChance!" gtr 0 (
 goto :lastchance
 )
 

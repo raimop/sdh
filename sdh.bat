7ab52ba712b3f7c9424b9cf43aca6dcea6deb4ff
raimop <ray@tlu.ee>
2018-11-23 00:41:00 +0200

    v1.1

--- sdh.bat
+++ sdh.bat
@@ -1,27 +1,48 @@
 @echo off & setlocal enabledelayedexpansion
 :start
 cls
-echo Shutdown helper v1.0 by Raimo (Tallinn University OS subject homework 18 Nov 2018^)
-echo For future use, writing: "sdh a" (a as a paramater^) will abort any planned shutdowns
-echo Can also write "sdh /?" for help
+echo Shutdown helper v1.1 by Raimo (Tallinn University OS subject homework 18 Nov 2018, updated 21 Nov 2018^)
+echo Press ENTER to continue with the script
+echo To abort any previous planned shutdowns, write: a
+echo For help, write: /?
 echo.
 
-if "%1" equ "a" (
+:initial
+set /p initial="Your wish is my command: "
+
+if "%initial%" equ "a" (
+echo Shutdown abort initiated successfully, unless shutdown prompted an error one line below this one.
+echo.
 shutdown -a
-echo.
-echo Shutdown abort initiated successfully, unless shutdown prompted own error one line above this one
-goto :oef
+goto :lastchance
 )
 
-if "%1" equ "/?" (
+if "%initial%" equ "/?" (
 echo.
-echo To start the script, simply write "sdh" with no parameters
-echo To abort any planned shutdowns, simply write "sdh a"
-echo To prompt this help, simply write "sdh /?"
+echo To start the script, simply double-click sdh.bat
+echo To abort any planned shutdowns, write: a
+echo To prompt this help again, write: /?
+echo To continue with SDH (Shutdown Helper^), simply press Enter key
 echo.
-goto :oef
+goto :initial
 )
 
+if "%initial%" equ "" (
+echo Enter pressed 
+goto :mainpart
+)
+
+if "%initial%" lss "" (
+echo less than ""
+goto :start
+)
+
+if "%initial%" gtr "" (
+echo greater than ""
+goto :start
+)
+
+:mainpart
 for /l %%i in (0, 1, 12) do (
 SET /a time[%%i]=300*%%i
 )
@@ -45,33 +66,37 @@ goto :select
 
 
 for /l %%i in (0, 1, 12) do (
-if !choice! equ %%i ( shutdown -s -t !time[%%i]! -c "Shutdown in !time[%%i]! seconds by SDH script (by Raimo)" )
+if !choice! equ %%i ( 
+SET /a seconds=300*%%i
+SET /a minutes=!seconds!/60
+echo.
+echo You chose %%i, will shutdown in !seconds! seconds (!minutes! minutes^)
+shutdown -s -t !time[%%i]! -c "Shutdown in !time[%%i]! seconds by SDH script (by Raimo)" 
+)
 )
 
-:abortquestion
-echo If you want to abort your option and start over, press 1, otherwise 0
+:lastchance
+echo If you want to re-run this script, write: yes
+echo If you are done and want to exit, press Enter
 
-set /p lastChance="Select abort number: "
-echo.
-if !lastChance! == 1 (
-shutdown -a
-echo.
-echo Shutdown abort initiated successfully, unless shutdown prompted own error one line above this one
-echo Starting over (continuing automatically in 20 seconds^)
-timeout /t 20
+set /p lastChance="Choice: "
+
+if "%lastChance%" == "yes" (
 goto :start
 )
-if !lastChance! == 0 (
+
+if "%lastChance%" == "" (
+echo Goodbye^^!
+timeout /t 5
 goto :oef
 )
-if !lastChance! lss 0 (
-echo You entered a number lesser than 0, returning to abort question
-goto :abortquestion
+
+if "%lastChance%" lss "" (
+goto :lastchance
 )
-if !lastChance! gtr 1 (
-echo You entered a number greater than 1, returning to abort question
-goto :abortquestion
+
+if "%lastChance%" gtr "" (
+goto :lastchance
 )
 
 :oef
-echo Goodbye^^!
